3b45b95dd987f4bd0b93b536103d3d1378b2832e
Antoine L <antoine.ligerot@outlook.fr>
2023-04-03 11:54:51 +0200

    premier test unitaire

---
+++ src/test/java/testeur.java
@@ -0,0 +1,31 @@
+import com.sun.source.tree.AssertTree;
+import fr.diginamic.enumerations.Saison;
+import org.junit.Test;
+
+import static fr.diginamic.enumerations.Saison.AUTOMNE;
+import static fr.diginamic.enumerations.Saison.valueOfLibelle;
+import static org.junit.Assert.assertEquals;
+
+
+public class testeur {
+    //non pasant
+
+    @Test
+    public void valeurLieblle(){
+        Saison saisonTest = valueOfLibelle("AUTOMNE");
+        assertEquals(saisonTest,AUTOMNE);
+    }
+//passant
+    @Test
+    public void valeurLieblle1(){
+        Saison saisonTest = valueOfLibelle("Automne");
+        assertEquals(saisonTest,AUTOMNE);
+    }
+    //non passant
+    @Test
+    public void valeurLieblle2(){
+        Saison saisonTest = valueOfLibelle("Printemps");
+        assertEquals(saisonTest,AUTOMNE);
+    }
+
+}
